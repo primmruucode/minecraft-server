80cb4886311990bdca37e3eb719a803fa0f04297
primmruucode <113852611+primmruucode@users.noreply.github.com>
2024-06-24 19:20:05 +0700

    Update tag.sh

--- script/tag.sh
+++ script/tag.sh
@@ -3,6 +3,7 @@ cd /home/mycroftsewer/minecraft-server
 ls
 git config --global --add safe.directory /home/mycroftsewer/minecraft-server
 git checkout -b test_branch
+touch jojo.txt
 git add .
 git commit --amend -m "update test2"
 git push --set-upstream origin test_branch
