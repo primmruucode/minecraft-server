7ef389b29c284a6ae1d18998899974fdd6a52668
primmruucode <113852611+primmruucode@users.noreply.github.com>
2024-06-24 18:41:05 +0700

    Update tag.sh

--- script/tag.sh
+++ script/tag.sh
@@ -2,5 +2,6 @@ echo Hello world
 cd /home/mycroftsewer/minecraft-server
 ls
 git config --global --add safe.directory /home/mycroftsewer/minecraft-server
-git fetch
-git pull
+git add .
+git commit -m "update test1"
+git push -u https://primmruucode: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@github.com/primmruucode/minecraft-server.git main
