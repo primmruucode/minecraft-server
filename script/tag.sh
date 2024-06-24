50b195d3c2eff45b2dbb74b46299d6560b373327
primmruucode <113852611+primmruucode@users.noreply.github.com>
2024-06-24 19:19:29 +0700

    Update tag.sh

--- script/tag.sh
+++ script/tag.sh
@@ -2,6 +2,7 @@ echo Hello world
 cd /home/mycroftsewer/minecraft-server
 ls
 git config --global --add safe.directory /home/mycroftsewer/minecraft-server
+git checkout -b test_branch
 git add .
-git commit--amend -m "update test2"
-git push 
+git commit --amend -m "update test2"
+git push --set-upstream origin test_branch
