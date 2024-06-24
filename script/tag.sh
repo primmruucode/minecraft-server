fe7fdeeade414cac2733efa4305d70379d98457d
Your Name <you@example.com>
2024-06-24 12:14:17 +0000

    jojo

--- script/tag.sh
+++ script/tag.sh
@@ -3,5 +3,5 @@ cd /home/mycroftsewer/minecraft-server
 ls
 git config --global --add safe.directory /home/mycroftsewer/minecraft-server
 git add .
-git commit -m "update test2"
-git push -u https://primmruucode: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@github.com/primmruucode/minecraft-server.git main
+git commit--amend -m "update test2"
+git push 
