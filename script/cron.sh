b485be38967d9641f5c3e2d0863233c789124d19
Your Name <you@example.com>
2024-07-19 21:56:50 +0700

    holyshit

---
+++ script/cron.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Navigate to your repository
+cd ~/biw2/minecraft-server/bukkit-sv
+
+# Ensure the repository is up-to-date
+# git pull origin main
+
+# Create a new tag with the current date and time
+git add .
+CM_NAME=$(date +"backup-%Y%m%d-%H%M%S")
+git commit -m $CM_NAME
+
+# Push the tag to the remote repository
+git push
---
+++ script/tag.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Navigate to your repository
+cd ~/biw2/minecraft-server/bukkit-sv
+
+# Ensure the repository is up-to-date
+# git pull origin main
+
+# Create a new tag with the current date and time
+TAG_NAME=$(date +"backup-%Y%m%d")
+git tag $TAG_NAME
+
+# Push the tag to the remote repository
+git push origin $TAG_NAME
